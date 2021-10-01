a265844e6d9aa57d7dd594f6f64122f87e5bee9c
Sara Caramaschi <caramaschisara@gmail.com>
2021-10-01 09:47:44 +0200

    Schermata0 + passaggio a Schermata1 vuota

---
+++ app/src/main/java/com/example/cpsproject/Schermata0.kt
@@ -0,0 +1,20 @@
+package com.example.cpsproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Schermata0 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schermata0)
+
+    // bottone da schermata 0 a schermata 1
+    val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+    btnSubmit.setOnClickListener {
+        val intent = Intent(this, Schermata1::class.java)
+        startActivity(intent)
+        }
+    }
+}

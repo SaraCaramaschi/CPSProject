7a1c0f4effea98874f4a9c984b38dfe7de98a5e7
Sara Caramaschi <caramaschisara@gmail.com>
2021-10-02 09:29:19 +0200

    Aggiunte schermate (vuote) connessione + lista pazienti con bottoni

---
+++ app/src/main/java/com/example/cpsproject/Connection.kt
@@ -0,0 +1,11 @@
+package com.example.cpsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Connection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection)
+    }
+}
--- app/src/main/java/com/example/cpsproject/Schermata1.kt
+++ app/src/main/java/com/example/cpsproject/Schermata1.kt
@@ -1,11 +1,32 @@
 package com.example.cpsproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Schermata1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schermata1)
+
+        // bottone LOG OUT
+        val btnLogOut= findViewById<Button>(R.id.btnLogOut)
+        btnLogOut.setOnClickListener {
+            val intent = Intent(this, Schermata0::class.java)
+            startActivity(intent)
+        }
+        // bottone LISTA PAZIENTI
+        val btnPatients = findViewById<Button>(R.id.btnPatients)
+        btnPatients.setOnClickListener {
+            val intent = Intent(this, PatientsList::class.java)
+            startActivity(intent)
+        }
+        // bottone CONNESSIONE PENNA
+        val btnConnection = findViewById<Button>(R.id.btnConnection)
+        btnConnection.setOnClickListener {
+            val intent = Intent(this, Connection::class.java)
+            startActivity(intent)
+        }
     }
 }

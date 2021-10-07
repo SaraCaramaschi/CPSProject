06b1c58bad67acd0cb5e8305dcb9930e0b1ea501
Sara Caramaschi <caramaschisara@gmail.com>
2021-10-07 13:09:39 +0200

    Aggiunti i 4 file (tra cui anche l'immagine 'user.png') per creare la lista pazienti

---
+++ app/src/main/java/com/example/cpsproject/PatientAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.cpsproject
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+class PatientAdapter: RecyclerView.Adapter<PatientAdapter.ViewHolder>() {
+    private var patientsName = arrayOf("Mario Rossi", "Sara Caram","Vichi Attols Dj")
+    private var patientsPhase = arrayOf("Phase1","Phase1","Phase2")
+    private var patientsItems = intArrayOf(R.drawable.user,R.drawable.user,R.drawable.user)
+    inner class ViewHolder(itemView: View ): RecyclerView.ViewHolder(itemView){
+
+        var itemImage: ImageView
+        var itemName: TextView
+        var itemPhase: TextView
+
+        init{
+            itemImage = itemView.findViewById(R.id.imageView)
+            itemName = itemView.findViewById(R.id.PatientName)
+            itemPhase = itemView.findViewById(R.id.PatientPhase)
+
+            itemView.setOnClickListener{
+                val position: Int = adapterPosition
+                Toast.makeText(itemView.context, "you clicked on ${patientsName[position]} ", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatientAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_patient, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: PatientAdapter.ViewHolder, position: Int) {
+        holder.itemName.text = patientsName[position]
+        holder.itemPhase.text = patientsPhase[position]
+        holder.itemImage.setImageResource(patientsItems[position])
+    }
+
+    override fun getItemCount(): Int {
+       return patientsName.size
+    }
+}

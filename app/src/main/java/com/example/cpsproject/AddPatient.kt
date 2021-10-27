954929d36040f75c692908b0e21d38b7da7cb808
Sara Caramaschi <caramaschisara@gmail.com>
2021-10-27 11:07:32 +0200

    tutto ok

--- app/src/main/java/com/example/cpsproject/AddPatient.kt
+++ app/src/main/java/com/example/cpsproject/AddPatient.kt
@@ -13,7 +13,6 @@ class AddPatient : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_patient)
-
     }
 }
 

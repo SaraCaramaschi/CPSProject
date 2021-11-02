243e04c5628c8f69e66f113053ecacf28ffcee4e
Sara Caramaschi <caramaschisara@gmail.com>
2021-11-02 15:54:12 +0100

    non va ancoraaaa magari ho un problema io con il pc

--- app/src/main/java/com/example/cpsproject/managers/PatientsManager.kt
+++ app/src/main/java/com/example/cpsproject/managers/PatientsManager.kt
@@ -3,7 +3,7 @@ package com.example.cpsproject.managers
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import android.util.Log
 import com.example.cpsproject.model.Patient
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -12,9 +12,7 @@ import kotlinx.android.synthetic.main.activity_add_patient.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import timber.log.Timber
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
+import java.io.*
 
 
 object PatientsManager {
@@ -26,18 +24,22 @@ object PatientsManager {
 
     public fun createJson(patient: Patient) {
         val json = Json.encodeToString(patient)
-        Timber.d("This is the json data: $json") // NON LO STAMPA
+        Log.d("This is the json data:", json) // LO STAMPA OK SUPER !
 
+
+        val file = File(System.getProperty("user.home"), "paziente.js")
         var fileName = "paziente.js"
         var fileDirectory = "/Users/saracaramaschi/SecondoAnnoM/"
         var filepath = "/Users/saracaramaschi/SecondoAnnoM/paziente.js"
 
-/*
-            METODO 1: NON VA
-            var newfile = File(fileDirectory, fileName)
-            newfile.writeText(json) // UTF-8 (default)
 
-            METODO 2: NON VA
+            //METODO 1: NON VA, questo penso proprio non vada bene
+            /*
+            var newfile = File(fileDirectory, fileName)
+            newfile.writeText(json) // UTF-8 (default)*/
+
+            //METODO 2: NON VA
+        /*
             val file = File(fileDirectory, fileName)
             val fileOutputStream: FileOutputStream
             try {
@@ -60,11 +62,13 @@ object PatientsManager {
         }catch (e: Exception){
             e.printStackTrace()
         }*/
+        //METODO non so quale: NON VA
+        file.bufferedWriter().use { bw -> bw.write(json) }
 
-        //METODO 4:
+        //METODO 4: non va
         // https://medium.com/android-news/android-saving-model-object-in-shared-preferences-ce3c1d4f4573
         //Shared Preference field used to save and retrieve JSON string
-        lateinit var preferences: SharedPreferences
+        /*lateinit var preferences: SharedPreferences
 
         //Name of Shared Preference file
         private const val PREFERENCES_FILE_NAME = "PREFERENCES_FILE_NAME"
@@ -90,7 +94,27 @@ object PatientsManager {
             //Save that String in SharedPreferences
             preferences.edit().putString(key, jsonString).apply()
         }
+*/
+        /* METODO GINEVRA: non va (a me)
+        var newpatient = patientsList.last()
+        val gson= Gson()
+        val jsonObj = gson.toJson(newpatient)
+        //COME CREO FILE IN CUI SALVARE IL JSON? HO CREATO PACKAGE IN CPSPROJECT
+        File("JsonFiles").writeText(jsonObj)*/
 
+        // altro metodo
+        /*FileOutputStream(file).use { fos ->
+            OutputStreamWriter(fos, Charsets.UTF_8).use { osw ->
+                BufferedWriter(osw).use { bf -> bf.write(json) }
+            }
+        }*/
+
+
+        /*Timber.d(PatientsManager.patientsList.elementAt(0).name)
+    Timber.d(PatientsManager.patientsList.elementAt(0).surname)
+    Timber.d(PatientsManager.patientsList.elementAt(0).notes)
+    Timber.d(PatientsManager.patientsList.elementAt(0).taxcode)
+     */
 
 
     }

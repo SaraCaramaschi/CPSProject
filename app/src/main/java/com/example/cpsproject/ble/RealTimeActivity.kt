8dedabb7579856d4effb623afb99f373f245c916
Sara Caramaschi <caramaschisara@gmail.com>
2021-11-06 17:31:38 +0100

    aggiunta real time activity e layout (vuoti)

---
+++ app/src/main/java/com/example/cpsproject/ble/RealTimeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cpsproject.ble
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cpsproject.R
+
+class RealTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_real_time)
+
+    }
+}

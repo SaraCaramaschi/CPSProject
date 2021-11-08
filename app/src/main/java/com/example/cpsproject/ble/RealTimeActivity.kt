df0fa879f0b3ead6db2815d088debb78649a9dd6
alessandro bonacina <alessandro4.bonacina@mail.polimi.it>
2021-11-09 00:07:58 +0100

    Iniziato realtime ma non funziona

--- app/src/main/java/com/example/cpsproject/ble/RealTimeActivity.kt
+++ app/src/main/java/com/example/cpsproject/ble/RealTimeActivity.kt
@@ -1,13 +1,28 @@
 package com.example.cpsproject.ble
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.cpsproject.R
+import com.example.cpsproject.Schermata1
+import kotlinx.android.synthetic.main.activity_real_time.*
+import org.jetbrains.anko.find
 
 class RealTimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_real_time)
         // Riempire le text view del design con valori ottenuti da readBattery / readData
+
+        //bottone per iniziare a riempire la textview con i dati (=iniziare il real time)
+        val startrealtime = findViewById<Button>(R.id.Startrealtime)
+        Startrealtime.setOnClickListener {
+        //val dato =intent.getStringExtra("acc_x")
+            //textView6.text= dato
+        }
+
     }
 }

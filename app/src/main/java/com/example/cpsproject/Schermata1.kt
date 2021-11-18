e4fc911729d1c40c180b2162e16f72d406e9fc15
ilaria <ilaria.guadagnuolo@mail.polimi.it>
2021-11-18 17:29:22 +0100

    ila fa cose

--- app/src/main/java/com/example/cpsproject/Schermata1.kt
+++ app/src/main/java/com/example/cpsproject/Schermata1.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.cpsproject.ble.MainConnection
+import kotlinx.android.synthetic.main.activity_add_clinician.*
+import kotlinx.android.synthetic.main.activity_schermata0.*
 
 class Schermata1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schermata1)
+
+//PROVA PER CLINICO ILA MA I PROSSIMI DUE COMMENTI POSSONO ESSERE CANCELLATI
+      //  val tvClinician=findViewById<TextView>(R.id.tvClinician)
+       // tvClinician.setText("Doctor  ${etEmailLog} ")
 
         // bottone LOG OUT
         val btnLogOut= findViewById<Button>(R.id.btnLogOut)
---
+++ app/src/main/java/com/example/cpsproject/managers/ClinicianManager.kt
@@ -0,0 +1,14 @@
+package com.example.cpsproject.managers
+/*
+import android.content.Context
+import com.example.cpsproject.model.Clinician
+
+object ClinicianManager
+
+    public var clinicianlist: ArrayList<Clinician> = ArrayList()
+
+    public fun addPatient(patient: Clinician, context: Context) {
+        clinician.add(patient)
+        ClinicianManager.saveClinican(patient, context)
+    }
+}*/

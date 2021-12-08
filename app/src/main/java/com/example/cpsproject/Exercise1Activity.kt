835051821fa8f1124f7445f07fd7aaac6cd36324
Sara Caramaschi <caramaschisara@gmail.com>
2021-12-08 21:07:58 +0100

    funzioni startSession / fillSession

--- app/src/main/java/com/example/cpsproject/Exercise1Activity.kt
+++ app/src/main/java/com/example/cpsproject/Exercise1Activity.kt
@@ -1,17 +1,15 @@
 package com.example.cpsproject
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cpsproject.managers.PatientsManager
 import com.example.cpsproject.managers.PenManager
+import com.example.cpsproject.model.PenData
 import com.example.cpsproject.model.Session
 import com.punchthrough.blestarterappandroid.ble.ConnectionManager
 import kotlinx.android.synthetic.main.activity_exercise1.*
 import java.time.LocalDateTime
-import java.util.*
 
 class Exercise1Activity: AppCompatActivity() {
     lateinit var session: Session
@@ -19,7 +17,6 @@ class Exercise1Activity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise1)
-
         tvExDescription1.text = "Make the patient write a grocery list with at least 6 elements"
 
         btnStart1.setOnClickListener {
@@ -27,14 +24,14 @@ class Exercise1Activity: AppCompatActivity() {
             // Inizializzazione di una sessione:
             startSession()
         }
-
         btnStop1.setOnClickListener {
             ConnectionManager.StopOnBoard()
         }
-
         // PER PROVARE: Poi vediamo in che modo inserirlo
         btnDownloadEx1.setOnClickListener {
             ConnectionManager.download()
+            // TODO Otteniamo eventuale oggetto DATA come??
+            // fillSession(data, session)
         }
     }
 
@@ -44,5 +41,8 @@ class Exercise1Activity: AppCompatActivity() {
         session.datetime = LocalDateTime.now().toString()
         session.patientPos = PatientsManager.selectedPatient
 
+    }
+    private fun fillSession(data:PenData, session: Session) {
+        session.data = data
     }
 }
